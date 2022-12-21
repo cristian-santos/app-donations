0159438ffa9b45d5feb44e86d6a4c78948676c3e
cristian-santos <cristiansantosti@gmail.com>
2022-12-21 20:47:17 -0300

    Direcionando usuários para o whatsapp

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,4 +9,12 @@ export class Tab3Page {
 
   constructor() {}
 
+  whats() {
+    this.abrirUrl('https://api.whatsapp.com/send?phone=5579996321034&&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App!%20%20Gostaria%20de%20receber%20doacões');
+  }
+
+  abrirUrl(url: string) {
+    window.open(url, '_blank')?.focus();
+  }
+
 }
