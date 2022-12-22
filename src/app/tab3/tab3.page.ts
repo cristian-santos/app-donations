39bc9505799e0d3b5b29e7964f04fcbcd27a0ee0
cristian-santos <cristiansantosti@gmail.com>
2022-12-21 23:33:38 -0300

    Formulário na tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
+  form: any;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
 
   whats() {
-    this.abrirUrl('https://api.whatsapp.com/send?phone=5579996321034&&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App!%20%20Gostaria%20de%20receber%20doacões');
+  
+  }
+
+  enviar() {
+    let nome = this.form.get('nome').value;
+    let endereco = this.form.get('endereco').value;
+
+    // nome = 'Cristian';
+    // endereco = 'Avenida teste, numero 10';
+
+    this.abrirUrl(`https://api.whatsapp.com/send?phone=5579996321034&&text=Olá%20me%20chamo%20${nome},%20%20moro%20na%20${endereco}%20.%20Vim%20atraves%20do%20seu%20App!%20%20Gostaria%20de%20receber%20doacões`);
+
   }
 
   abrirUrl(url: string) {
