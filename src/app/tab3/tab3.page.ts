5d4414d9678e23abb636a86bb414ed0b0930ca2c
cristian-santos <cristiansantosti@gmail.com>
2022-12-23 20:20:26 -0300

    Redirecionar página ao ir pro whasapp

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -28,9 +28,8 @@ export class Tab3Page implements OnInit{
   enviar() {
     let nome = this.form.get('nome').value;
     let endereco = this.form.get('endereco').value;
-
     this.abrirUrl(`https://api.whatsapp.com/send?phone=5579996321034&&text=Olá%20me%20chamo%20${nome},%20%20moro%20na%20${endereco}.%20%20Vim%20atraves%20do%20seu%20App!%20%20Gostaria%20de%20receber%20doacões`);
-
+    this.ngOnInit();
   }
 
   abrirUrl(url: string) {
