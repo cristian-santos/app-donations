92b8f1d9ace0a3e054e0938b5f971ce9b9e76b89
cristian-santos <cristiansantosti@gmail.com>
2022-12-23 19:34:48 -0300

    Ajuste no direcionamento pra whatsapp

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -29,10 +29,7 @@ export class Tab3Page implements OnInit{
     let nome = this.form.get('nome').value;
     let endereco = this.form.get('endereco').value;
 
-    // nome = 'Cristian';
-    // endereco = 'Avenida teste, numero 10';
-
-    this.abrirUrl(`https://api.whatsapp.com/send?phone=5579996321034&&text=Olá%20me%20chamo%20${nome},%20%20moro%20na%20${endereco}%20.%20Vim%20atraves%20do%20seu%20App!%20%20Gostaria%20de%20receber%20doacões`);
+    this.abrirUrl(`https://api.whatsapp.com/send?phone=5579996321034&&text=Olá%20me%20chamo%20${nome},%20%20moro%20na%20${endereco}.%20%20Vim%20atraves%20do%20seu%20App!%20%20Gostaria%20de%20receber%20doacões`);
 
   }
 
